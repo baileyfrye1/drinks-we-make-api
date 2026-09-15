a096955db4e17c66b14bc41f36297d5f35e5769c
baileyfrye1 <baileyafrye@comcast.net>
2026-09-15 11:37:35 -0500

    Started outlining unit tests

--- tests/DrinksWeMake.Tests/Cocktails/GetOrCreateIngredientTests.cs
+++ tests/DrinksWeMake.Tests/Cocktails/GetOrCreateIngredientTests.cs
@@ -2,5 +2,9 @@ namespace DrinksWeMake.Tests.Cocktails;
 
 public class GetOrCreateIngredientTests
 {
-    
+   [Fact]
+   public async Task CreateNewIngredient_ReturnsCreatedIngredient()
+   {
+      await using var dbContext = TestDbContext.CreateDbContext();
+   }
 }
--- tests/DrinksWeMake.Tests/TestDbContext.cs
+++ tests/DrinksWeMake.Tests/TestDbContext.cs
@@ -1,6 +1,19 @@
+using DrinksWeMake.Api.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 namespace DrinksWeMake.Tests;
 
-public class TestDbContext
+public static class TestDbContext
 {
-    
+    private const string ConnectionString =
+        "Host=localhost;Port=5433;Database=drinks_we_make_test;Username=test;Password=test";
+    public static AppDbContext CreateDbContext()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseNpgsql(ConnectionString)
+            .Options;
+
+        return new AppDbContext(options);
+    }
 }
